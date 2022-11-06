854146e752f6862b200d2029727f64b39d80e045
Сидоркин А.А <Sidrsidsashok@mail.ru>
2022-11-07 00:37:25 +0700

    Colleagues migration to delegates

--- app/src/main/java/com/example/hrautomation/presentation/view/colleagues/ColleaguesAdapter.kt
+++ app/src/main/java/com/example/hrautomation/presentation/view/colleagues/ColleaguesAdapter.kt
@@ -1,43 +1,15 @@
 package com.example.hrautomation.presentation.view.colleagues
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.example.hrautomation.databinding.EmployeeRecyclerviewItemBinding
-import com.example.hrautomation.presentation.model.ColleagueItem
+import com.example.hrautomation.presentation.base.delegates.BaseListItem
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 
-class ColleaguesAdapter(private val onClickListener: OnColleagueClickListener) :
-    RecyclerView.Adapter<ColleaguesAdapter.ViewHolder>() {
-
-    private var dataSet: List<ColleagueItem> = emptyList()
-
-    class ViewHolder(val binding: EmployeeRecyclerviewItemBinding, clickAtPosition: (Int) -> Unit) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        init {
-            binding.root.setOnClickListener { clickAtPosition(adapterPosition) }
-        }
+class ColleaguesAdapter(onColleagueClickListener: OnColleagueClickListener) : ListDelegationAdapter<List<BaseListItem>>() {
+    init {
+        delegatesManager.addDelegate(ColleaguesItemAdapterDelegate(onColleagueClickListener))
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = EmployeeRecyclerviewItemBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return ViewHolder(binding) { onClickListener.onClick(dataSet[it]) }
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.employeeName.text = dataSet[position].name
-        holder.binding.employeePost.text = dataSet[position].post
-        holder.binding.employeeProject.text = dataSet[position].project
-    }
-
-    override fun getItemCount(): Int {
-        return dataSet.size
-    }
-
-    fun update(data: List<ColleagueItem>) {
-        dataSet = data
+    fun update(data: List<BaseListItem>) {
+        setItems(data)
         notifyDataSetChanged()
     }
 }
