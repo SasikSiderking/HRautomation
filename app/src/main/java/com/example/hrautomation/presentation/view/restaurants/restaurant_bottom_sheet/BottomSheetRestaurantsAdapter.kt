e2e2c0f6e8b18d73190de04f6f03757ac1b268ac
-_- <Sidrsidsashok@mail.ru>
2023-03-03 02:09:35 +0300

    DiffUtil

--- app/src/main/java/com/example/hrautomation/presentation/view/restaurants/restaurant_bottom_sheet/BottomSheetRestaurantsAdapter.kt
+++ app/src/main/java/com/example/hrautomation/presentation/view/restaurants/restaurant_bottom_sheet/BottomSheetRestaurantsAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.hrautomation.presentation.view.restaurants.restaurant_bottom_sheet
 
+import androidx.recyclerview.widget.DiffUtil
 import com.example.hrautomation.presentation.base.delegates.BaseListItem
 import com.example.hrautomation.presentation.view.restaurants.list.OnRestaurantClickListener
+import com.example.hrautomation.utils.DiffUtilCallback
 import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 
 class BottomSheetRestaurantsAdapter(onRestaurantClickListener: OnRestaurantClickListener) :
@@ -13,7 +15,10 @@ class BottomSheetRestaurantsAdapter(onRestaurantClickListener: OnRestaurantClick
     }
 
     fun update(data: List<BaseListItem>) {
+        val oldItems = getItems().orEmpty()
+        val diffUtilCallback = DiffUtilCallback(oldItems, data)
+        val diffResult = DiffUtil.calculateDiff(diffUtilCallback)
         setItems(data)
-        notifyDataSetChanged()
+        diffResult.dispatchUpdatesTo(this)
     }
 }
---
+++ app/src/main/java/com/example/hrautomation/utils/DiffUtilCallback.kt
@@ -0,0 +1,29 @@
+package com.example.hrautomation.utils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.hrautomation.presentation.base.delegates.BaseListItem
+
+class DiffUtilCallback<T : BaseListItem> constructor(
+    private val oldList: List<T>,
+    private val newList: List<T>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem == newItem
+    }
+}
