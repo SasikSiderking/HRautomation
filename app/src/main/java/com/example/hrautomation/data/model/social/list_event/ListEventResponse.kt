b1764b160e4efd71706e5c73d6c0957ccdbc925b
-_- <Sidrsidsashok@mail.ru>
2023-05-31 18:17:51 +0300

    date formatting crutch

--- app/src/main/java/com/example/hrautomation/data/model/social/list_event/ListEventResponse.kt
+++ app/src/main/java/com/example/hrautomation/data/model/social/list_event/ListEventResponse.kt
@@ -1,11 +1,9 @@
 package com.example.hrautomation.data.model.social.list_event
 
-import java.util.Date
-
 class ListEventResponse(
     val id: Long,
     val name: String,
-    val date: Date,
+    val date: String,
     val pictureUrl: String?,
     val format: String
 )
--- app/src/main/java/com/example/hrautomation/utils/date/DateUtils.kt
+++ app/src/main/java/com/example/hrautomation/utils/date/DateUtils.kt
@@ -1,18 +1,22 @@
 package com.example.hrautomation.utils.date
 
-import org.joda.time.DateTime
-import org.joda.time.format.DateTimeFormat
 import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 object DateUtils {
+
+    private const val INPUT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss"
 
     const val PATTERN = "dd.MM.yyyy"
 
     private const val DAY_MONTH_PATTERN = "d MMMM"
 
-    private const val DAY_MONTH_LOCALE_PATTERN = "d MMMM, HH:mm X"
+    private const val DAY_MONTH_TIME_PATTERN = "d MMMM, HH:mm"
 
     fun formatDate(date: Date): String {
         val formatter = SimpleDateFormat(PATTERN, Locale("ru", "RU"))
@@ -20,17 +24,25 @@ object DateUtils {
     }
 
     fun formatDateToDayMonth(date: Date): String {
-        val formatter = SimpleDateFormat(DAY_MONTH_PATTERN, Locale("ru", "RU"))
-        return formatter.format(date)
+        val zonedDateTime = ZonedDateTime.ofInstant(date.toInstant(), ZoneId.of("Europe/Moscow"))
+        val formatter = DateTimeFormatter.ofPattern(DAY_MONTH_PATTERN, Locale("ru", "RU"))
+        return zonedDateTime.format(formatter)
     }
 
     fun formatDateToDayMonthAndLocale(date: Date): String {
-        val formatter = SimpleDateFormat(DAY_MONTH_LOCALE_PATTERN, Locale("ru", "RU"))
-        return formatter.format(date)
+        val zonedDateTime = ZonedDateTime.ofInstant(date.toInstant(), ZoneId.of("Europe/Moscow"))
+        val formatter = DateTimeFormatter.ofPattern(DAY_MONTH_TIME_PATTERN, Locale("ru", "RU"))
+        return zonedDateTime.format(formatter) + " МСК"
     }
 
-    fun parseDate(string: String): DateTime {
-        val formatter = DateTimeFormat.forPattern(PATTERN)
-        return formatter.parseDateTime(string)
+    fun parseDate(string: String): Date {
+        val isoFormat = SimpleDateFormat(INPUT_PATTERN, Locale("ru", "RU"))
+        isoFormat.timeZone = TimeZone.getTimeZone("UTC")
+        val date = isoFormat.parse(string)
+        if (date != null) {
+            return date
+        } else {
+            throw IllegalStateException("Unparsable date")
+        }
     }
 }
